01a2bdca1a0140aece5a76d9abbe43e02ff3f50d
minhhai044 <tmhai2004@gmail.com>
2024-03-14 11:17:08 +0700

    update

---
+++ admin/header.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ADMIN</title>
+    <link rel="stylesheet" href="../cssfile/cssadmin.css">
+  </head>
+  <body>
+    <div class="container">
+      <header>
+        <div class="header_img">
+          <img
+            src="https://tse4.mm.bing.net/th?id=OIP.QdkBFiUZG4SMw0C7krIbrwHaHa&pid=Api&P=0&h=180"
+            alt=""
+          />
+        </div>
+        <!-- <div class="header_search">
+          <input type="text" />
+          <button>Tìm kiếm</button>
+        </div>
+        <div class="header_dndk">
+          <a href="">Đăng nhập</a>
+          <a href="">Đăng ký</a>
+        </div> -->
+      </header>
+      <nav>
+        <ul>
+          <li><a href="#">Trang chủ</a></li>
+          <li><a href="#">Danh mục</a></li>
+          <li><a href="#">Sản phẩm</a></li>
+          <li><a href="#">Hỏi đáp</a></li>
+          <li><a href="#">Hỗ trợ</a></li>
+        </ul>
+      </nav>
+      <!-- <div class="slideshow">
+        <div class="slideleft">
+          <img
+            src="https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:10/plain/https://dashboard.cellphones.com.vn/storage/samsung-s23-th1-right.png"
+            alt=""
+          />
+          <img
+            src="https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:10/plain/https://dashboard.cellphones.com.vn/storage/right-banner-ipad-gen9-new-th2.jpg"
+            alt=""
+          />
+          <img
+            src="https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:10/plain/https://dashboard.cellphones.com.vn/storage/right sv.png"
+            alt=""
+          />
+        </div>
+        <div class="slideshow-container">
+          <div class="mySlides fade">
+            <img src="img/3.jpg" />
+          </div>
+
+          <div class="mySlides fade">
+            <img src="img/4.jpg" />
+          </div>
+
+          <div class="mySlides fade">
+            <img src="img/5.jpg" />
+          </div>
+        </div>
+        <div class="slideright">
+          <img
+            src="https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:10/plain/https://dashboard.cellphones.com.vn/storage/samsung-s23-th1-right.png"
+            alt=""
+          />
+          <img
+            src="https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:10/plain/https://dashboard.cellphones.com.vn/storage/right-banner-ipad-gen9-new-th2.jpg"
+            alt=""
+          />
+          <img
+            src="https://cdn2.cellphones.com.vn/insecure/rs:fill:690:300/q:10/plain/https://dashboard.cellphones.com.vn/storage/right sv.png"
+            alt=""
+          />
+        </div>
+      </div> -->
+
+      <!-- <article>
+        <img
+          src="https://img.tgdd.vn/imgt/f_webp,fit_outside,quality_100/https://cdn.tgdd.vn/2024/03/banner/Desk-1200x100.png"
+          alt=""
+        />
+        <div class="hot_top_sanpham">
+          <img
+            src="https://cdn2.cellphones.com.vn/x/media/catalog/product/h/o/hot-sale-gia-soc-11-03-2024.png"
+            alt=""
+          />
+          <div class="top_sanpham">
+            <div class="top_sanpham_1"></div>
+            <div class="top_sanpham_2"></div>
+            <div class="top_sanpham_3"></div>
+            <div class="top_sanpham_4"></div>
+            <div class="top_sanpham_5"></div>
+          </div>
+        </div>
+        <div class="sanpham">
+          <div class="sanpham_1"></div>
+          <div class="sanpham_2"></div>
+          <div class="sanpham_3"></div>
+          <div class="sanpham_4"></div>
+          <div class="sanpham_5"></div>
+        </div>
+      </article> -->
+    </div>
+  </body>
+
+  <!-- <script>
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+      slides[slideIndex - 1].style.display = "block";
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+  </script> -->
+</html>
